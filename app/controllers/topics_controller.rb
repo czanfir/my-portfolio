9997799027c1c59672bfeec3b935df92f61d2da3
Cristi Zanfir <zanfircristian92@gmail.com>
2018-03-30 15:13:27 +0000

    Fix bug with topics not being set on topic show call

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,8 +6,10 @@ class TopicsController < ApplicationController
 
     if logged_in?(:site_admin)
       @blogs = @topic.blogs.recent.page(params[:page]).per(5)
+      @topics = Topic.all
     else
       @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
+      @topics = Topic.with_blogs
     end
   end
 end
