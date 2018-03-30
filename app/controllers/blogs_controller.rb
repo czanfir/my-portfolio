ae6b49eb83c20856ba0b3b9eda4b7b55a902fe4b
Cristi Zanfir <zanfircristian92@gmail.com>
2018-03-30 11:56:10 +0000

    Added custom scope for recent blogs and hide draft posts from non-admin users

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,11 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    if logged_in?(:site_admin)
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
     @page_title = "Cristian Zanfir's Blog"
   end
 
@@ -67,10 +71,12 @@ class BlogsController < ApplicationController
   def toggle_status
     if @blog.draft?
       @blog.published! 
+      new_status = "published"
     elsif @blog.published?
       @blog.draft!
+      new_status = "draft"
     end
-    redirect_to blogs_url
+    redirect_to blogs_url, notice: "Post status has been updated to #{new_status}."
   end
 
   private
