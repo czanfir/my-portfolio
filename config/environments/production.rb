1316fdc6b411ba9c5f7e4e5b9c80670be9d4397e
Cristi Zanfir <cristian.zanfir@unifiedpost.com>
2018-05-23 11:38:10 +0300

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = [ 'https://cristizanfir-portfolio.herokuapp.com', 'http://cristizanfir-portfolio.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
